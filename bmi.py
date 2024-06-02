927606aa8e8a9d33eb628459a64d0076e27edc54
santopaul <120390816+santopaul@users.noreply.github.com>
2024-06-02 10:38:41 -0400

    version 1.1

--- bmi.py
+++ bmi.py
@@ -1,20 +1,48 @@
-from pyfiglet import figlet_format
+#from pyfiglet import figlet_format
 import os
 import sys
-from colorama import Fore, Style
-try:
-    #banner creation
-    if sys.platform == "linux" or sys.platform == "linux2":
-        os.system("clear")
-    elif sys.platform == "win32":
-        os.system("cls")
-    elif sys.platform == "darwin":
-        os.system("clear")
-    print(Fore.BLUE + figlet_format("WELCOME MR.SANTO", font="banner"))
-    print(Style.RESET_ALL)
-except:
-    print("Banner Error!")
+#from colorama import Fore, Style
+# try:
+#     #banner creation
+#     if sys.platform == "linux" or sys.platform == "linux2":
+#         os.system("clear")
+#     elif sys.platform == "win32":
+#         os.system("cls")
+#     elif sys.platform == "darwin":
+#         os.system("clear")
+#     print(Fore.BLUE + figlet_format("WELCOME MR.SANTO", font="banner"))
+#     print(Style.RESET_ALL)
+# except:
+#     print("Banner Error!")
 
+#colors used
+blue='\033[94m'
+red='\033[31m'
+yellow='\033[93m'
+lgreen='\033[92m'
+clear='\033[0m'
+pink='\033[95m'
+bold='\033[01m'
+
+#banner
+print(blue+"""
+ '##:::::'##:'########:'##::::::::'######:::'#######::'##::::'##:'########:     
+ ##:'##: ##: ##.....:: ##:::::::'##... ##:'##.... ##: ###::'###: ##.....::     
+ ##: ##: ##: ##::::::: ##::::::: ##:::..:: ##:::: ##: ####'####: ##:::::::     
+ ##: ##: ##: ######::: ##::::::: ##::::::: ##:::: ##: ## ### ##: ######:::     
+ ##: ##: ##: ##...:::: ##::::::: ##::::::: ##:::: ##: ##. #: ##: ##...::::     
+ ##: ##: ##: ##::::::: ##::::::: ##::: ##: ##:::: ##: ##:.:: ##: ##:::::::     
+. ###. ###:: ########: ########:. ######::. #######:: ##:::: ##: ########:     
+:...::...:::........::........:::......::::.......:::..:::::..::........::     
+'##::::'##:'########::::::::'######:::::'###::::'##::: ##:'########::'#######::
+ ###::'###: ##.... ##::::::'##... ##:::'## ##::: ###:: ##:... ##..::'##.... ##:
+ ####'####: ##:::: ##:::::: ##:::..:::'##:. ##:: ####: ##:::: ##:::: ##:::: ##:
+ ## ### ##: ########:::::::. ######::'##:::. ##: ## ## ##:::: ##:::: ##:::: ##:
+ ##. #: ##: ##.. ##:::::::::..... ##: #########: ##. ####:::: ##:::: ##:::: ##:
+ ##:.:: ##: ##::. ##::'###:'##::: ##: ##.... ##: ##:. ###:::: ##:::: ##:::: ##:
+ ##:::: ##: ##:::. ##: ###:. ######:: ##:::: ##: ##::. ##:::: ##::::. #######::
+..:::::..::..:::::..::...:::......:::..:::::..::..::::..:::::..::::::.......:::
+"""+clear)
 #Intro to bmi calculation
 print("Welcome to BMI Calculator!")
 print("Enter the operation number that you want.")
@@ -46,40 +74,36 @@ try:
         weight=float(input("Enter weight in Pounds: "))
         height=float(input("Enter height in inches: "))
         bmi=703*bmi_calculation(weight,height)
+    elif operation == 3:  
+        #Readmore section
+        print(bold+"Body Mass Index (BMI): BMI is a tool proved to be useful for health care professionals in analyzing the risk for future cardiovascular, endocrine and other diseases such as: diabetes, arthritis, liver disease, several types of cancer (such as those of the breast, colon, and prostate), high blood pressure (hypertension), high cholesterol, sleep apnea, metabolic syndrome.")
+        print("It is important to note that BMI can miss accuracy when it comes to adults who are pregnant, have high muscle mass, for children or elderly. Which is why this data alone can not be used to make any diagnosis."+clear)
     else:
-        print(Fore.RED + "Sorry invalid Input!")
-        print(Style.RESET_ALL)
+        print(red + "Sorry invalid Input!"+clear)
+
 except:
     print("Operation Number error!")
 #Diagnostics
 if (bmi<16):
-    print("your bmi is: ",str(bmi))
-    print(Fore.GREEN + "You're severely underweight.")
+    print(pink+"your bmi is: ",str(bmi)+clear)
+    print(lgreen + "You're severely underweight."+clear)
 elif (16<=bmi<=18.4):
-    print("your bmi is: ",str(bmi))
-    print(Fore.GREEN + "You're underweight.")
+    print(pink+"your bmi is: ",str(bmi)+clear)
+    print(lgreen + "You're underweight."+clear)
 elif (18.5<=bmi<=24.9):
-    print("your bmi is: ",str(bmi))
-    print(Fore.GREEN + "You're normal.")
+    print(pink+"your bmi is: ",str(bmi)+clear)
+    print(lgreen + "You're normal."+clear)
 elif (25<=bmi<=29.9):
-    print("your bmi is: ",str(bmi))
-    print(Fore.LIGHTRED_EX + "You're overweight.")
+    print(pink+"your bmi is: ",str(bmi)+clear)
+    print(red + "You're overweight."+clear)
 elif (30<=bmi<=34.9):
-    print("your bmi is: ",str(bmi))
-    print(Fore.RED + "You're moderately obese.")
+    print(pink+"your bmi is: ",str(bmi)+clear)
+    print(red + "You're moderately obese."+clear)
 elif (35<=bmi<=39.9):
-    print("your bmi is: ",str(bmi))
-    print(Fore.RED + "You're severely obese.")
+    print(pink+"your bmi is: ",str(bmi)+clear)
+    print(red + "You're severely obese."+clear)
 elif (bmi>=40):
-    print("your bmi is: ",str(bmi))
-    print(Fore.RED + "You're morbidly obese.")
+    print(pink+"your bmi is: ",str(bmi)+clear)
+    print(red + "You're morbidly obese."+clear)
 
-print(Style.RESET_ALL)
-
-#Readmore section
-if operation == 3:
-    print("Body Mass Index (BMI): BMI is a tool proved to be useful for health care professionals in analyzing the risk for future cardiovascular, endocrine and other diseases such as: diabetes, arthritis, liver disease, several types of cancer (such as those of the breast, colon, and prostate), high blood pressure (hypertension), high cholesterol, sleep apnea, metabolic syndrome.")
-    print("It is important to note that BMI can miss accuracy when it comes to adults who are pregnant, have high muscle mass, for children or elderly. Which is why this data alone can not be used to make any diagnosis.")
-
-print(Fore.LIGHTYELLOW_EX + "Thank you for using me. For more tools visit: https://santopaul.github.io/Santo/")
-print(Style.RESET_ALL)
+print(yellow +bold+ "Thank you for using me. For more tools visit: https://santopaul.github.io/Santo/"+clear)
