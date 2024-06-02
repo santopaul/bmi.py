f6d65d56ef3aedefa0f7fc738baa798f820006f3
Santo Paul <santopaul2005@gmail.com>
2024-06-01 21:55:31 -0400

    first commit

---
+++ bmi.py
@@ -0,0 +1,85 @@
+from pyfiglet import figlet_format
+import os
+import sys
+from colorama import Fore, Style
+try:
+    #banner creation
+    if sys.platform == "linux" or sys.platform == "linux2":
+        os.system("clear")
+    elif sys.platform == "win32":
+        os.system("cls")
+    elif sys.platform == "darwin":
+        os.system("clear")
+    print(Fore.BLUE + figlet_format("WELCOME MR.SANTO", font="banner"))
+    print(Style.RESET_ALL)
+except:
+    print("Banner Error!")
+
+#Intro to bmi calculation
+print("Welcome to BMI Calculator!")
+print("Enter the operation number that you want.")
+print("1. Metric System.(Kgs, Meters)")
+print("2. Imperial System.(Pounds, inches)")
+print("3. Read about BMI")
+
+try:
+    #select operation number
+    operation=int(input("\n Operation number: "))
+
+    #function for calculation a/bsqr
+    def bmi_calculation(a,b):
+        bsqr=b*b
+        res=float(a/bsqr)
+        return res
+    
+    #taking values based on system and units
+    if operation == 1:
+        print("Metric System calculation selected!")
+        global weight
+        weight=float(input("Enter weight in Kgs: "))
+        global heights
+        height=float(input("Enter height in meters: "))
+        global bmi
+        bmi=bmi_calculation(weight,height)
+    elif operation == 2:
+        print("Imperial System calculation selected!")
+        weight=float(input("Enter weight in Pounds: "))
+        height=float(input("Enter height in inches: "))
+        bmi=703*bmi_calculation(weight,height)
+    else:
+        print(Fore.RED + "Sorry invalid Input!")
+        print(Style.RESET_ALL)
+except:
+    print("Operation Number error!")
+#Diagnostics
+if (bmi<16):
+    print("your bmi is: ",str(bmi))
+    print(Fore.GREEN + "You're severely underweight.")
+elif (16<=bmi<=18.4):
+    print("your bmi is: ",str(bmi))
+    print(Fore.GREEN + "You're underweight.")
+elif (18.5<=bmi<=24.9):
+    print("your bmi is: ",str(bmi))
+    print(Fore.GREEN + "You're normal.")
+elif (25<=bmi<=29.9):
+    print("your bmi is: ",str(bmi))
+    print(Fore.LIGHTRED_EX + "You're overweight.")
+elif (30<=bmi<=34.9):
+    print("your bmi is: ",str(bmi))
+    print(Fore.RED + "You're moderately obese.")
+elif (35<=bmi<=39.9):
+    print("your bmi is: ",str(bmi))
+    print(Fore.RED + "You're severely obese.")
+elif (bmi>=40):
+    print("your bmi is: ",str(bmi))
+    print(Fore.RED + "You're morbidly obese.")
+
+print(Style.RESET_ALL)
+
+#Readmore section
+if operation == 3:
+    print("Body Mass Index (BMI): BMI is a tool proved to be useful for health care professionals in analyzing the risk for future cardiovascular, endocrine and other diseases such as: diabetes, arthritis, liver disease, several types of cancer (such as those of the breast, colon, and prostate), high blood pressure (hypertension), high cholesterol, sleep apnea, metabolic syndrome.")
+    print("It is important to note that BMI can miss accuracy when it comes to adults who are pregnant, have high muscle mass, for children or elderly. Which is why this data alone can not be used to make any diagnosis.")
+
+print(Fore.LIGHTYELLOW_EX + "Thank you for using me. For more tools visit: https://santopaul.github.io/Santo/")
+print(Style.RESET_ALL)
